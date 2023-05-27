3c6c470b8390878d0270e0edbd8a718547be7e92
hedihmida <hedihmida2@gmail.com>
2023-05-27 13:20:50 +0100

    signup popup

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -2,7 +2,14 @@
 import Link from "next/link";
 import { useState } from "react";
 
+
 const SignupPage = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+const handlePopupClose = () => {
+  setShowPopup(false);
+  window.location.reload(); 
+};
   const [email,setEmail] = useState("");
   const [password,setPassword] = useState("");
 
@@ -27,8 +34,10 @@ const SignupPage = () => {
     })
     console.log(data);
     if(data.ok){
-      alert("Account Created Successfully")
+     
       await data.json();
+      setShowPopup(true);
+      
       console.log(data);
     }
   }catch(error) {
@@ -40,6 +49,8 @@ const SignupPage = () => {
   
   return (
     <>
+    {
+    
       <section className="relative z-10 overflow-hidden pt-36 pb-16 md:pb-20 lg:pt-[180px] lg:pb-28">
         <div className="container">
           <div className="-mx-4 flex flex-wrap">
@@ -94,7 +105,7 @@ const SignupPage = () => {
                   </p>
                   <span className="hidden h-[1px] w-full max-w-[60px] bg-body-color sm:block"></span>
                 </div>
-                {/* <form> */}
+               
                   <div className="mb-8">
                     <label
                       htmlFor="name"
@@ -166,8 +177,8 @@ const SignupPage = () => {
                             >
                               <path
                                 d="M10.0915 0.951972L10.0867 0.946075L10.0813 0.940568C9.90076 0.753564 9.61034 0.753146 9.42927 0.939309L4.16201 6.22962L1.58507 3.63469C1.40401 3.44841 1.11351 3.44879 0.932892 3.63584C0.755703 3.81933 0.755703 4.10875 0.932892 4.29224L0.932878 4.29225L0.934851 4.29424L3.58046 6.95832C3.73676 7.11955 3.94983 7.2 4.1473 7.2C4.36196 7.2 4.55963 7.11773 4.71406 6.9584L10.0468 1.60234C10.2436 1.4199 10.2421 1.1339 10.0915 0.951972ZM4.2327 6.30081L4.2317 6.2998C4.23206 6.30015 4.23237 6.30049 4.23269 6.30082L4.2327 6.30081Z"
-                                fill="#3056D3"
-                                stroke="#3056D3"
+                                fill="#FFFFFF"
+                                stroke="#FFFFFF"
                                 strokeWidth="0.4"
                               />
                             </svg>
@@ -193,7 +204,7 @@ const SignupPage = () => {
                       Sign up
                     </button>
                   </div>
-                {/* </form> */}
+              
                 <p className="text-center text-base font-medium text-body-color">
                   Already using Doctors?
                   <Link href="/signin" className="text-primary hover:underline">
@@ -262,7 +273,24 @@ const SignupPage = () => {
           </svg>
         </div>
       </section>
+    
+  }
+    {showPopup && (
+      <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="bg-white dark:bg-dark max-w-[500px] rounded-md p-8 shadow-lg">
+          <h2 className="text-2xl font-bold mb-4">Confirmation</h2>
+          <p>Your account has been created successfully!</p>
+          <button
+            className="mt-4 px-4 py-2 bg-primary text-white font-medium rounded hover:bg-opacity-80"
+            onClick={handlePopupClose}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    )}
     </>
+    
   );
 };
 
